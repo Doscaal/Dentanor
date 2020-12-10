1ebc0408024082dc9c0775630a44591994a9d6b0
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2020-12-10 10:10:58 +0100

    remove inherit on process barcode

--- Doscaal/Dentanor_profile/wizard/__init__.py
+++ Doscaal/Dentanor_profile/wizard/__init__.py
@@ -2,6 +2,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 # -*- coding: utf-8 -*-
 
-from . import stock_barcodes_read
+# from . import stock_barcodes_read
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
