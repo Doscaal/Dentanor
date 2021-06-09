23a806f612a129926dc8d3ef914b9e2933dda1e7
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2021-06-10 00:14:05 +0200

    modification envoi email

--- Doscaal/Dentanor_profile/models/sale_order.py
+++ Doscaal/Dentanor_profile/models/sale_order.py
@@ -8,3 +8,19 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     date_order = fields.Datetime(readonly=False, track_visibility='onchange')
+
+    def _find_mail_template(self, force_confirmation_template=False):
+        template_id = super(SaleOrder, self)._find_mail_template(
+            force_confirmation_template)
+        if force_confirmation_template or (
+                self.state == 'sale' and not self.env.context.get(
+                    'proforma', False)):
+            template_id = int(self.env['ir.config_parameter'].sudo().get_param(
+                'sale.default_confirmation_template_%s' % self.company_id.id))
+            template_id = self.env['mail.template'].search(
+                [('id', '=', template_id)]).id
+            if not template_id:
+                template_id = self.env['ir.model.data'].xmlid_to_res_id(
+                    'sale.mail_template_sale_confirmation',
+                    raise_if_not_found=False)
+        return template_id
