629018b9ed712de6c98dccd103892a8339cc93c1
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2021-06-17 18:36:19 +0200

    modification calcul purchase_price

--- Doscaal/Dentanor_profile/models/sale_order_line.py
+++ Doscaal/Dentanor_profile/models/sale_order_line.py
@@ -1,11 +1,25 @@
 # Copyright 2020 Doscaal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    @api.model
+    def _get_purchase_price(self, pricelist, product, product_uom, date):
+        frm_cur = self.env.company.currency_id
+        to_cur = pricelist.currency_id
+        purchase_price = product.margin_cost
+        if product_uom != product.uom_id:
+            purchase_price = product.uom_id._compute_price(
+                purchase_price, product_uom)
+        price = frm_cur._convert(
+            purchase_price, to_cur,
+            self.order_id.company_id or self.env.company,
+            date or fields.Date.today(), round=False)
+        return {'purchase_price': price}
 
     def _compute_margin(self, order_id, product_id, product_uom_id):
         frm_cur = self.env.company.currency_id
