d33c399c1962c801a862a76d88e0656cb9be3865
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2021-07-13 01:15:58 +0200

    changement du domain

--- Doscaal/Dentanor_profile/models/__init__.py
+++ Doscaal/Dentanor_profile/models/__init__.py
@@ -15,5 +15,6 @@ from . import res_users
 from . import stock_quant
 from . import product_product
 from . import sale_order_line
+from . import hr_expense
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
